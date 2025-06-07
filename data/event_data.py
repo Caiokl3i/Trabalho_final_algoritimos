387b09cf4fa991658937ae9c2b2bfbbb8010b0a4
Caiokl3i <caio.victor.santos12@gmail.com>
2025-06-06 22:35:19 -0400

    feat: adiciona arquivos base de eventos e participantes

---
+++ data/event_data.py
@@ -0,0 +1,44 @@
+from participant_data import participants_list
+
+events_list = [
+    {
+        'nome': 'Workshop de IA Generativa',
+        'data': '10-08-2025',
+        'tema_central': 'Inteligência Artificial',
+        'participantes_event': [
+            participants_list[0], participants_list[3]
+        ]
+    },
+    {
+        'nome': 'Maratona Web Dev',
+        'data': '04-07-2025',
+        'tema_central': 'Web',
+        'participantes_event': [
+            participants_list[0], participants_list[4]
+        ]
+    },
+    {
+        'nome': 'Palestra de Segurança Digital',
+        'data': '25-08-2025',
+        'tema_central': 'Segurança da Informação',
+        'participantes_event': [
+            participants_list[1], participants_list[2]
+        ]
+    },
+    {
+        'nome': 'Encontro Cloud e DevOps',
+        'data': '03-09-2025',
+        'tema_central': 'DevOps',
+        'participantes_event': [
+            participants_list[1], participants_list[2]
+        ]
+    },
+    {
+        'nome': 'Tech Talks: UX/UI Trends',
+        'data': '07-10-2025',
+        'tema_central': 'UX/UI',
+        'participantes_event': [
+            participants_list[4]
+        ]
+    }
+]
