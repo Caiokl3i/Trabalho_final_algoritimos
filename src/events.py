3839ba6825ffb19f85153c5cf63974b71d54db40
Caiokl3i <caio.victor.santos12@gmail.com>
2025-06-14 18:26:49 -0400

    feat: adição da listagem de participantes por evento

--- src/events.py
+++ src/events.py
@@ -1,6 +1,22 @@
 from event_data import events_list
 
-def show_events():
-    for item in events_list:
-        print(f'Evento: {item["nome"]} \n  Data: {item["data"]}\n  Tema: {item["tema_central"]}\n')
+def show_event_list():
+    for i, item in enumerate(events_list):
+        print(f'{i + 1} - Evento: {item["nome"]}')
+        print(f'  Data: {item["data"]}')
+        print(f'  Tema: {item["tema_central"]}\n')
 
+def show_participants_event():
+    show_event_list()
+    
+    print('--> VERIFICAR PARTICIPANTES DO EVENTO <--\n')
+    n = int(input('Digite o número do evento para ver os participantes (Apenas Números) \n'))
+    
+    if 1 <= n <= len(events_list):
+        print(f'\nOs participantes do evento {events_list[n - 1]["nome"]} são: \n')
+        
+        for participants in events_list[n - 1]["participantes_event"]:
+            print(f'Nome: {participants["nome"]}')
+            print(f'Email: {participants["email"]} \n')
+    else:
+        pass
