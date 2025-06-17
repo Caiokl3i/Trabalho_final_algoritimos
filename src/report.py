83af35614ca3a34457813603923b709bad7369af
Caiokl3i <caio.victor.santos12@gmail.com>
2025-06-16 21:31:27 -0400

    chore: reorganiza posição de comentário no código

--- src/report.py
+++ src/report.py
@@ -1,15 +1,15 @@
 from collections import Counter
 from event_data import events_list
 
-'''
-    Funcionalidade ainda em desenvolvimento.
-Counter - para contar quantos eventos tem.
-dict - para tranformar o que o Counter retorna para um dicionario.
-.item() - para retornar chave e valor para serem usados na parte visivel.
-    Iniciando uso de list comprehension
-'''
-
 def report_frequency():
+    '''
+    - Funcionalidade ainda em desenvolvimento.
+    Counter - para contar quantos eventos tem.
+    dict - para tranformar o que o Counter retorna para um dicionario.
+    .item() - para retornar chave e valor para serem usados na parte visivel.
+    - Iniciando uso de list comprehension
+    '''
+    
     temas = [tema['tema_central'] for tema in events_list]
 
     contagem = dict(Counter(temas))
