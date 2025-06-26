f0621ce8e5316ea0e01f3c66fa8647a1a9f36edd
Caiokl3i <caio.victor.santos12@gmail.com>
2025-06-25 23:35:30 -0400

    feat: implementa função para editar os dados do participante

--- src/participants.py
+++ src/participants.py
@@ -1,6 +1,7 @@
 from participant_data import participants_list
+from InquirerPy import inquirer
 
-def search_code_partcipant():
+def search_participant_for_cpf():
     '''
     Funcionalidade ainda em desenvolvimento.
     Falta ajustar a o layout do menu.
@@ -9,10 +10,50 @@ def search_code_partcipant():
     print('\n ---> VERIFICAÇÃO DOS DADOS DOS PARTICPANTES <--- \n')
     cpf = int(input('Digite o cpf do participante: \n'))
     
-    for i, participants in enumerate(participants_list):
+    for participants in participants_list:
         if cpf == participants["cpf"]:
             print(f'\nInformações do participante: \n')
             print(f' CPF: {participants["cpf"]}')
             print(f' Nome: {participants["nome"]}')
             print(f' Email: {participants["email"]}')
-            print(f' Prefereências Temáticas: {participants["preferencias_tematicas"]}')
+            print(f' Prefereências Temáticas: {participants["preferencias_tematicas"]}\n')
+
+def edit_participant_data():
+    '''
+    - Funcionalidade em desenvolvimento.
+    Exibe os dados do participante atraves do CPF e permite editar os
+    dados por um menu interativo.
+    '''
+    
+    cpf = int(input('Digite o cpf do participante: \n'))
+    
+    for participants in participants_list:
+        if cpf == participants["cpf"]:
+            print(f'\nInformações do participante: \n')
+            print(f' CPF: {participants["cpf"]}')
+            print(f' Nome: {participants["nome"]}')
+            print(f' Email: {participants["email"]}')
+            print(f' Preferências Temáticas: {participants["preferencias_tematicas"]}\n')
+    
+    print('\n----- INSIRA OS NOVOS DADOS DO PARTICIPANTE -----\n')
+    
+    for participant in participants_list:
+        if cpf == participant['cpf']:
+            nome = inquirer.text(
+                message='NOME: ',
+                validate=lambda result: len(result) > 0 or 'É obrigatório preencher esse campo!'
+            ).execute()
+            email = inquirer.text(
+                message='EMAIL: ',
+                validate=lambda result: len(result) > 0 or 'É obrigatório preencher esse campo!'
+            ).execute()
+            themes_preffer = inquirer.text(
+                message='PREFERÊNCIA TEMÁTICA: ',
+                validate=lambda result: len(result) > 0 or 'É obrigatório preencher esse campo!'
+            ).execute()
+
+            participant['nome'] = nome
+            participant['email'] = email
+            participant['preferencias_tematicas'] = themes_preffer
+            
+            print(participant)
